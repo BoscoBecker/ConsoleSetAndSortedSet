8a561e175ae3325bbd0aca43088fed4ebef0f2eb
João Bosco Dos Reis Becker <joao.jbosco@gmail.com>
2024-11-21 08:35:19 -0300

    Indentation 😇

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace ConsoleSetAndSortedSet {
+    internal abstract class Program {
+        private static void Main() {
+            var a = new SortedSet<int>() { 0, 1, 2, 3, 4, 5, 6 ,7};
+            var b = new SortedSet<int>() { 7, 8, 9, 10, 10, 12, 11 };
+            PrintCollection(a);
+            
+            // UNION 🧑‍🤝‍🧑
+            var c = new SortedSet<int>(a);
+                c.UnionWith(b);
+            PrintCollection(c);
+            
+            //  INTERSECTION 🤝
+            var d = new SortedSet<int>(a);
+                d.IntersectWith(b);
+            PrintCollection(d);
+            
+            // EXCEPT WITH⛔
+            var e = new SortedSet<int>(a);
+               e.ExceptWith(b);
+            PrintCollection(e);
+        }
+
+        private static void PrintCollection<T>(IEnumerable<T> Collection) {
+            foreach (var Obj in Collection) {
+                Console.WriteLine($"{Obj} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
